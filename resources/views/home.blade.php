d47b73641b193f7dd9667bee7c17b2fefb71768a
Hitesh Patil <hiteshpatil912@gmail.com>
2022-03-31 15:29:11 +0530

    my posty

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Hiii Hitesh -->
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+        Home
+</div>
+    </div>
+
+@endsection
